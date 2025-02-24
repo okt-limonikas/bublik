8d5d1a040578ee61b68becc1314bfee47981e5ba
Danil Kostromin <danil.kostromin@icloud.com>
2025-02-24 03:16:48 +0300

    WIP Add entry point config creating
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-echo "Collect static files"
+echo "Collect Static Files"
 python manage.py collectstatic --noinput
 
-echo "Apply database migrations"
+echo "Apply Database Migrations"
 python manage.py makemigrations
 python manage.py migrate
 
-# echo "=============================================="
-# echo "Ownership and Permissions of /app/bublik:"
-# ls -l /app/bublik
-# echo "=============================================="
+echo "Initialize Configurations"
+python manage.py initialize_configs
+
+# Create superuser
+echo "Creating Superuser"
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+SUPERUSER_EMAIL = '${DJANGO_SUPERUSER_EMAIL:-admin@example.com}'
+SUPERUSER_PASSWORD = '${DJANGO_SUPERUSER_PASSWORD:-admin}'
+if not User.objects.filter(email=SUPERUSER_EMAIL).exists():
+    User.objects.create_superuser(
+        email=SUPERUSER_EMAIL,
+        password=SUPERUSER_PASSWORD,
+        is_active=True
+    )
+    print(f'Superuser created with email: {SUPERUSER_EMAIL}')
+else:
+    print(f'Superuser with email {SUPERUSER_EMAIL} already exists.')
+EOF
 
 exec "$@"
